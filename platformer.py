596cdc1fcbedbe5fe9339ab5b39f212eee26374a
Arie Rampelmann-Ramirez <arr@Aries-Air.localdomain>
2024-05-09 18:43:20 -0400

    thingsprite

--- platformer.py
+++ platformer.py
@@ -4,22 +4,56 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = 'hijump'
 
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+
+
 class Platformer(arcade.Window):
 
     def __init__(self):
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
+        self.wall_list = None
+        self.player_list = None
+
+        self.player_sprite = None 
+
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
-        pass
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)    
+
+        image_source = ":resources:images/topdown_tanks/tank_blue.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.wall_list.append(wall)
+
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+        for coordinate in coordinate_list:
+            # Add a crate on the ground
+            wall = arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
+            )
+            wall.position = coordinate
+            self.wall_list.append(wall)
 
     def on_draw(self):
         """Render the screen."""
         self.clear()
         # Code to draw the screen goes here
+        self.wall_list.draw()
+        self.player_list.draw()
 
 def main():
     window = Platformer()
