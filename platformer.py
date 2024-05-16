21221c06cc33c5553ffacb7091e829150f3e24b7
Arie Rampelmann-Ramirez <arr@Aries-Air.localdomain>
2024-05-16 19:00:53 -0400

    hi do scene thigny

--- platformer.py
+++ platformer.py
@@ -7,6 +7,7 @@ SCREEN_TITLE = 'hijump'
 CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
 
+PLAYER_MOVEMENT_SPEED = 5
 
 class Platformer(arcade.Window):
 
@@ -14,29 +15,34 @@ class Platformer(arcade.Window):
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
+        self.scene = None
+
         self.wall_list = None
         self.player_list = None
 
         self.player_sprite = None 
 
+        self.physics_engine = None
+
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
-        """Set up the game here. Call this function to restart the game."""
-        self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList(use_spatial_hash=True)    
+        self.scene = arcade.Scene()
+
+        self.scene.add_sprite_list('Player')
+        self.scene.add_sprite_list('Walls', use_spatial_hash=True)
 
         image_source = ":resources:images/topdown_tanks/tank_blue.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 128
-        self.player_list.append(self.player_sprite)
+        self.scene.add_sprite("Player", self.player_sprite)
 
         for x in range(0, 1250, 64):
             wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
             wall.center_x = x
             wall.center_y = 32
-            self.wall_list.append(wall)
+            self.scene.add_sprite('Walls', wall)
 
         coordinate_list = [[512, 96], [256, 96], [768, 96]]
 
@@ -46,14 +52,46 @@ class Platformer(arcade.Window):
                 ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
             )
             wall.position = coordinate
-            self.wall_list.append(wall)
+            self.scene.add_sprite('Walls', wall)
+
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player_sprite, self.scene.get_sprite_list("Walls")
+        )
+
 
     def on_draw(self):
         """Render the screen."""
         self.clear()
-        # Code to draw the screen goes here
-        self.wall_list.draw()
-        self.player_list.draw()
+
+        self.scene.draw()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        
+
+        self.physics_engine.update()
 
 def main():
     window = Platformer()
