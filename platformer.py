91aa99a42e7d8328adc41ef018fe52af6d929342
Arie Rampelmann-Ramirez <arr@Aries-Air.localdomain>
2024-06-20 18:01:53 -0400

    added gravity

--- platformer.py
+++ platformer.py
@@ -9,6 +9,9 @@ TILE_SCALING = 0.5
 
 PLAYER_MOVEMENT_SPEED = 5
 
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
+
 class Platformer(arcade.Window):
 
     def __init__(self):
@@ -24,19 +27,23 @@ class Platformer(arcade.Window):
 
         self.physics_engine = None
 
+        self.camera = None 
+
         arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
 
     def setup(self):
+        self.camera = arcade.Camera(self.width, self.height)
         self.scene = arcade.Scene()
 
         self.scene.add_sprite_list('Player')
         self.scene.add_sprite_list('Walls', use_spatial_hash=True)
 
-        image_source = ":resources:images/topdown_tanks/tank_blue.png"
+        image_source = "images/personsprite.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 128
         self.scene.add_sprite("Player", self.player_sprite)
+       
 
         for x in range(0, 1250, 64):
             wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
@@ -54,8 +61,8 @@ class Platformer(arcade.Window):
             wall.position = coordinate
             self.scene.add_sprite('Walls', wall)
 
-        self.physics_engine = arcade.PhysicsEngineSimple(
-            self.player_sprite, self.scene.get_sprite_list("Walls")
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Walls"]
         )
 
 
@@ -63,13 +70,16 @@ class Platformer(arcade.Window):
         """Render the screen."""
         self.clear()
 
+        self.camera.use()
+
         self.scene.draw()
 
     def on_key_press(self, key, modifiers):
         """Called whenever a key is pressed."""
 
         if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
         elif key == arcade.key.DOWN or key == arcade.key.S:
             self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
         elif key == arcade.key.LEFT or key == arcade.key.A:
@@ -79,19 +89,31 @@ class Platformer(arcade.Window):
 
     def on_key_release(self, key, modifiers):
 
-        if key == arcade.key.UP or key == arcade.key.W:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.DOWN or key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.LEFT or key == arcade.key.A:
+        if key == arcade.key.LEFT or key == arcade.key.A:
             self.player_sprite.change_x = 0
         elif key == arcade.key.RIGHT or key == arcade.key.D:
             self.player_sprite.change_x = 0
 
+    def center_camera_to_player(self):
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (
+            self.camera.viewport_height / 2
+        )
+
+        # Don't let camera travel past 0
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
+
     def on_update(self, delta_time):
         
-
         self.physics_engine.update()
+
+        self.center_camera_to_player()
 
 def main():
     window = Platformer()
